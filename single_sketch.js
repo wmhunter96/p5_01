1cda7da9f0b3ea9e82836f40186ae1b1b03e91e6
Hunter Rodriguez <acehunter96@gmail.com>
2020-09-27 15:25:26 -0700

    abanndon b/c cant figure out how to copy triangular areas in p5js

--- single_sketch.js
+++ single_sketch.js
@@ -1,13 +1,62 @@
 // Single-sketch example
 
 function setup (){
-  createCanvas (800, 600);
+  bg = loadImage('bg.jpg');
+  createCanvas (800, 800);
+
+
+
+  let sides = ['A','B','C','D'];
+  side1 = random(sides);
+  side2 = random(sides);
+  while (side1 == side2){
+    side2 = random(sides);
+  }
+
+  switch (side1) {
+    case 'A':
+      startx = random(0,800);
+      starty = 0;
+      break;
+    case 'B':
+      startx = 800;
+      starty = random(0,800);
+      break;
+    case 'C':
+      startx = random(0,800);
+      starty = 800;
+      break;
+    case 'D':
+      startx = 0;
+      starty = random(0,800);
+      break;
+  }
+  switch (side2) {
+    case 'A':
+      endx = random(0,800);
+      endy = 0;
+      break;
+    case 'B':
+      endx = 800;
+      endy = random(0,800);
+      break;
+    case 'C':
+      endx = random(0,800);
+      endy = 800;
+      break;
+    case 'D':
+      endx = 0;
+      endy = random(0,800);
+      break;
+  }
 }
 
 function draw(){
-  background(100);
+  background(bg);
   fill(255);
   noStroke();
-  rectMode(CENTER);
-  rect(mouseX, mouseY, 50, 50);
+  stroke(255);
+
+  line(startx,starty,endx,endy);
 }
+
